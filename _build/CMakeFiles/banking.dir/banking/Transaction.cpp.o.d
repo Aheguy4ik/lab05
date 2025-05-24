fed107ff50cbb4c84065b2e64659be1d3671ed90
Aheguy4ik <matveiluzkov@inbox.ru>
2025-05-24 15:17:02 +0300

    Build

--- _build/CMakeFiles/banking.dir/banking/Transaction.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/banking.dir/banking/Transaction.cpp.o: \
- /home/aheguy/lab05/banking/Transaction.cpp /usr/include/stdc-predef.h \
- /home/aheguy/lab05/banking/Transaction.h \
- /home/aheguy/lab05/banking/Account.h
