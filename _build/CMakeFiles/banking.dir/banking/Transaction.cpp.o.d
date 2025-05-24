3cb088e7c3c8fb10179223cff1a9ad2d14ac515c
Aheguy4ik <matveiluzkov@inbox.ru>
2025-05-24 14:23:55 +0300

    FIX

---
+++ _build/CMakeFiles/banking.dir/banking/Transaction.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/banking.dir/banking/Transaction.cpp.o: \
+ /home/aheguy/lab05/banking/Transaction.cpp /usr/include/stdc-predef.h \
+ /home/aheguy/lab05/banking/Transaction.h \
+ /home/aheguy/lab05/banking/Account.h
